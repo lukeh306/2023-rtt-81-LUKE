7d4a431870dbc7830dbd69b3d354870b3f7fa7c2
lukeh306 <lukeholmes189@gmail.com>
2023-10-05 19:54:42 -0500

    Map Demo

---
+++ homework/src/collections/MapExample.java
@@ -0,0 +1,52 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MapExample {
+	public static void main(String[] args) {
+	
+			Map<String,Integer> stringMap = new HashMap<>();
+			
+			stringMap.put("one", 1);
+			stringMap.put("two", 2);
+			
+			Integer one = stringMap.get("one");
+			Integer two = stringMap.get("two");
+			
+			System.out.println(stringMap.get("one"));
+			System.out.println(stringMap.get("two"));
+			
+			stringMap.put("one", 1000);
+			
+			System.out.println(stringMap.get("one"));
+			
+			Map<String,String> strings = new HashMap<>();
+			
+			strings.put("one", "This is a single item");
+			strings.put("two", "This is two things");
+			
+			Map<Integer, String> numbers = new HashMap <>();
+			numbers.put(1, "1st");
+			numbers.put(2, "2nd");
+			numbers.put(3, "3rd");
+			numbers.put(4, "4th");
+			
+			System.out.println("==============");
+			
+			
+			for ( Integer key : numbers.keySet() ) {
+				System.out.println(numbers.get(key));
+				
+				String value = numbers.get(key);
+				System.out.println(key + " =  " + value);
+				
+				
+			}
+}	
+	
+	
+	
+
+}
