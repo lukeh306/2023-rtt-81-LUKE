8cb02cdc5f7aafdef4a1dd40840fc310ca134b37
lukeh306 <lukeholmes189@gmail.com>
2023-10-06 08:16:02 -0500

    For Loops

---
+++ homework/src/loops/LoopDemoOne.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class LoopDemoOne {
+	public static void main(String[] args) {
+	    int n = 7;
+	    // for loop  
+	    for (int i = 1; i <= n; ++i) {
+	      System.out.println("Java is fun");
+	    }
+	  }
+	}
+
+
