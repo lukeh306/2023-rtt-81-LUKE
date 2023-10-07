81570c704cfab24517812da85806072ff3a863ee
lukeh306 <lukeholmes189@gmail.com>
2023-10-07 18:24:41 -0500

    Test

---
+++ homewrk/src/javastring/ToUpperCase.java
@@ -0,0 +1,9 @@
+package javastring;
+
+public class ToUpperCase{ 
+	  public static void main(String args[]){ 
+	    String s1="hello how are you"; 
+	    String s1upper=s1.toUpperCase(); 
+	    System.out.println(s1upper); 
+	}}
+
